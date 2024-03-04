54e552f9bcdc4fc395c6e91bf8237fd989b6d94e
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-04 18:53:20 -0500

    added walmart logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+	Grid2X2,
+	Heart,
+	LayoutGrid,
+	Search,
+	ShoppingCart,
+	User,
+} from "lucide-react";
 
 function Header() {
   return (
-    <header>
-			<Link href='/'>
+    <header className="flex bg-walmart">
+			<Link href='/' className="">
 				<Image
-					src="https://links.papareact.com/xsi"
+					src="https://links.papareact.com/yur"
 					alt="Logo"
 					width={150}
 					height={150} 
 						/>
 			</Link>
+			<form className="flex items-center bg-white rounded-full w-full flex-1">
+				<input 
+					type='text' 
+					placeholder="Search Everything..."
+					className="flex-1"
+				/>
+				<button>
+					<Search className="rounded-full h-10"
+					
+					/>
+				</button>
+			</form>
     </header>
   )
 }
