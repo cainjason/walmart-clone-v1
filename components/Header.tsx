52588bda74e4bb2a592520eb0085d0fdfb91272c
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-04 19:39:31 -0500

    added links and made it responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ import {
 
 function Header() {
   return (
-    <header className="flex bg-walmart">
+    <header className="flex bg-walmart px-10 py-7 space-x-5">
 			<Link href='/' className="">
 				<Image
 					src="https://links.papareact.com/yur"
@@ -24,14 +24,25 @@ function Header() {
 				<input 
 					type='text' 
 					placeholder="Search Everything..."
-					className="flex-1 px-4 rounded-l-full outline-none"
+					className="flex-1 px-4 rounded-l-full outline-none placeholder:text-sm"
 				/>
-				<button>
-					<Search className="rounded-full h-10"
+				<button type="submit">
+					<Search className="rounded-full h-10 px-2 w-10 bg-yellow-400 cursor-pointer"
 					
 					/>
 				</button>
 			</form>
+
+			<div>
+				<Link
+					href={"/"}
+					className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+				>
+				<Grid2X2 size={20} />
+				<p>Departments</p>
+				</Link>
+			</div>
+
     </header>
   )
 }
